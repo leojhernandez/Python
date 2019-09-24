02a1872b93e8cef4fda6a2daab3bae8d0d462959
Leonardo Hernandez <35539591+leojhernandez@users.noreply.github.com>
2019-09-24 16:13:50 -0400

    Update bubble_sort_7_19_2019.py

--- Python Puzzles/bubble_sort_7_19_2019.py
+++ Python Puzzles/bubble_sort_7_19_2019.py
@@ -9,7 +9,7 @@ def bubblesort(lst):
     for index in range(passes_left):
       if lst[index] > lst[index +1]: 
         lst[index], lst[index +1] = lst[index +1], lst[index]
-   return lst
+  return lst
 
 l = [27,0,23,90,71,77]
 print(bubblesort(l))
