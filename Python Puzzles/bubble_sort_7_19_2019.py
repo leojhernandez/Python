813a36cb9d8c487c0f3a409eb01ef3c1307eff6f
Leonardo Hernandez <35539591+leojhernandez@users.noreply.github.com>
2019-09-24 16:10:17 -0400

    Create bubble_sort_7_19_2019.py

---
+++ Python Puzzles/bubble_sort_7_19_2019.py
@@ -0,0 +1,16 @@
+# Bubblesort 101
+
+# def: Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly steps through 
+# the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated 
+# until the list is sorted.
+
+def bubblesort(lst):
+  for passes_left in range(len(lst)-1,0,1):
+    for index in range(passes_left):
+      if lst[index] > lst[index +1]: 
+        lst[index], lst[index +1] = lst[index +1], lst[index]
+   return lst
+
+l = [27,0,23,90,71,77]
+print(bubblesort(l))
+
